04212a23dc7370788c6847d59505aa8bffdda346
Merlin Patterson <merlinpatt@gmail.com>
2016-09-27 19:27:43 -0400

    Switch to ES2015 syntax, strinigy error message

--- external-api.js
+++ external-api.js
@@ -1,49 +1,24 @@
 ExternalApi = {};
 
-ExternalApi.call = function externalApiCall(args, callback) {
-  var errorCode;
-  var errorMessage;
-  var method = (args.method || 'GET').toLowerCase();
-  var url = args.url;
-  var errorProperties = args.errorProperties;
-  var httpOptions = args.httpOptions || {};
+ExternalApi.call = ({httpOptions = {}, method = 'GET', url}, callback) => {
+  const errorCode = 500;
 
   if (! url) {
-    errorMessage = 'Cannot make HTTP request without a URL';
+    const errorMessage = 'Cannot make HTTP request without a URL';
+    callback(new Meteor.Error(errorCode, errorMessage));
   }
-  if (! HTTP.hasOwnProperty(method)) {
-    errorMessage = 'Cannot make HTTP request without an HTTP method';
-  }
-  if (httpOptions && typeof httpOptions !== 'object') {
-    errorMessage = 'Cannot make HTTP request if options are not an object';
-  }
-  if (errorMessage) {
-    errorCode = 500;
-    callback(new Meteor.Error(errorCode, errorMessage), null);
+  if (! HTTP.hasOwnProperty(method.toLowerCase())) {
+    const errorMessage = `HTTP does not have the method ${method.toLowerCase()}`;
+    callback(new Meteor.Error(errorCode, errorMessage));
   }
 
   try {
-    var response = HTTP.call(method, url, httpOptions).data;
-
+    const response = HTTP.call(method.toLowerCase(), url, httpOptions).data;
     callback(null, response);
   }
-  catch (e) {
-    // If the API responded with an error message and a payload
-    if (e.response) {
-      if (errorProperties.code && errorProperties.message) {
-        errorCode = e.response.data[errorProperties.code];
-        errorMessage = e.response.data[errorProperties.message];
-      }
-      else {
-        errorCode = 500;
-        errorMessage = 'Cannot give an API specific error without errorProperties defining code and message';
-      }
-    }
-    else {
-      errorCode = 500;
-      errorMessage = 'Cannot access the API';
-    }
-
+  catch (error) {
+    // if the API responded with an error message and a payload, stringify the payload
+    const errorMessage = error.response ? JSON.stringify(error.response.data) : 'Cannot access the API';
     callback(new Meteor.Error(errorCode, errorMessage), null);
   }
 };
--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'merlin:external-api',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Async and sync functions to handle calling external APIs',
   git: 'https://github.com/merlinpatt/external-api',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  api.versionsFrom('1.3.2.2');
+  api.use('ecmascript');
   api.use('http', ['server']);
 
   api.addFiles('external-api.js');
